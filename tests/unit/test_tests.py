477863cb08fe42e495d50d49298cab00c2325e55
Han <60923573+ligerzero-ai@users.noreply.github.com>
2025-07-22 23:27:59 +0200

    Initial commit

---
+++ tests/unit/test_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+import pyiron_module_template
+
+
+class TestVersion(unittest.TestCase):
+    def test_version(self):
+        version = pyiron_module_template.__version__
+        print(version)
+        self.assertTrue(version.startswith("0"))
---
+++ update_module_name.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Mac users: You [may first need to install gnu-sed](https://github.com/MigleSur/GenAPI/issues/8)
+
+module_name="pyiron_IntendedModuleName"
+
+for file in .binder/postBuild \
+            .github/ISSUE_TEMPLATE/*.md \
+            docs/conf.py \
+            docs/index.rst \
+            notebooks/example.ipynb \
+            pyiron_module_template/_version.py \
+            tests/unit/test_tests.py \
+            .coveragerc \
+            .gitattributes \
+            MANIFEST.in \
+            pyproject.toml
+do
+  sed -i "s/pyiron_module_template/${module_name}/g" ${file}
+  sed -i "s/======================/${rst_delimit}/g" ${file}
+done
+
+
+git mv pyiron_module_template ${module_name}
+
+rm update_module_name.sh
